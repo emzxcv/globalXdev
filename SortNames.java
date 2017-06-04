930b83b6345b90c2a96bb979087e86224ad51ee3
emzxcv <emilyha17@gmail.com>
2017-06-04 22:14:37 +1000

    Update and rename sortNames.java to SortNames.java

--- SortNames.java
+++ SortNames.java
@@ -7,40 +7,40 @@ import java.util.Scanner;
 public class SortNames {
 
 	public static void main(String[] args) throws IOException {
-	
-		File inFile = new File (args[0]);
+
+		File inFile = new File(args[0]);
 		sorter(inFile);
 		System.out.println("Finished: created names-sorted.txt");
 	}
-	
-public static void sorter(File inFile) throws IOException {
-		
+
+	public static void sorter(File inFile) throws IOException {
+
 		outputFile(SortNames.sortList(inFile));
 
 	}
+
 	public static ArrayList<Name> sortList(File inFile) throws FileNotFoundException {
 		ArrayList<Name> listName = new ArrayList<Name>();
 
-		Scanner sc = new Scanner (inFile);
+		Scanner sc = new Scanner(inFile);
 
-	    while (sc.hasNextLine())
-	    {
-	    	Name names = new Name(sc.nextLine());
+		while (sc.hasNextLine()) {
+			Name names = new Name(sc.nextLine());
 			listName.add(names);
-	    }	
-	    sc.close();
+		}
+		sc.close();
 		Collections.sort(listName);
 		return listName;
 	}
-	
+
 	public static void outputFile(ArrayList<Name> list) throws IOException {
-		File outFile = new File ("names-sorted.txt");
-		FileWriter fWriter = new FileWriter (outFile);
-		PrintWriter pWriter = new PrintWriter (fWriter);
-		for(Name str: list) {
-		System.out.println(str.toString());	
-		  pWriter.write(str.toString());
-		  pWriter.write("\n");
+		File outFile = new File("names-sorted.txt");
+		FileWriter fWriter = new FileWriter(outFile);
+		PrintWriter pWriter = new PrintWriter(fWriter);
+		for (Name str : list) {
+			System.out.println(str.toString());
+			pWriter.write(str.toString());
+			pWriter.write("\n");
 		}
 		pWriter.close();
 	}
